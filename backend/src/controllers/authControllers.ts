6f1f3f27c04d2259f46c01c061ab3159f87ff4d0
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2025-08-17 14:20:56 +0530

    complete the authetication code

--- backend/src/controllers/authControllers.ts
+++ backend/src/controllers/authControllers.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { User } from "../models/User";
+import process from "process";
+import { Session } from "../models/session";
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -32,6 +34,69 @@ export const register = async (req: Request, res: Response) => {
       message: "User registered SuccessFully.",
     });
   } catch (err: any) {
-    res.status(500).json({ message: "Internal Server Error" ,err:err.message});
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", err: err.message });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({ message: "email and password are reuired" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        message: "invalid email and password",
+      });
+    }
+    const Ispassword = await bcrypt.compare(password, user.password);
+    if (!Ispassword) {
+      return res.status(401).json({
+        message: "Invalid email and password",
+      });
+    }
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET || "your-secret-key",
+      { expiresIn: "24h" }
+    );
+    const expiresAt = new Date();
+    expiresAt.setHours(expiresAt.getHours() + 24);
+    const session = new Session({
+      userId: user._id,
+      token,
+      expiresAt,
+      deviceInfo: req.headers["user-agent"],
+    });
+    await session.save();
+    res.json({
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      token,
+      message: "Login SuccessFully",
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: "Server Error",
+      error,
+    });
+  }
+};
+
+export const logout = async (req: Request, res: Response) => {
+  try {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+    if (token) {
+      await Session.deleteOne({ token });
+    }
+    res.json({ message: "Logged out successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
   }
 };
