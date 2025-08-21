65809711d5df7cd534879cc26c7eb726587e584c
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2025-08-21 23:42:33 +0530

    made the auth for the App

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,11 +1,17 @@
-import {Router} from "express";
-import {register,login,logout} from "../controllers/authControllers";
+import { Router, Request, Response } from "express";
+import { register, login, logout } from "../controllers/authControllers";
+import { auth } from "../middleware/auth";
 
 // middleware -check point  in your app flow they can chnage in both incoming and outgoing reponse and request
 
+const router = Router();
 
 
+router.post("/register",register);
+router.post("/login",login);
+router.post("/logout",auth,logout);
 
-
-
-const router=Router();
+router.get("/me",auth, async (req: Request, res: Response) => {
+    res.json({ user: req.user });
+})
+export default router;
